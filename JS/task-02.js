e386162d9eb5ef9b07fe82de68ed9e40a6b9e39c
khomasl <gymnasium.yv@gmail.com>
2021-05-26 07:30:24 +0300

    task-01-03

---
+++ JS/task-02.js
@@ -0,0 +1,7 @@
+// console.log('hw-02');
+// const shouldConfirm = confirm('{jnbnt ghjl');
+// console.log(shouldConfirm);
+// const quantity = prompt();
+// console.log(quantity);
+// console.log(2 ** 100);
+console.log(Math.random()*100);
---
+++ JS/task-03.js
@@ -0,0 +1,4 @@
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message;
+const password = prompt('Введите пароль');
+alert(message);
